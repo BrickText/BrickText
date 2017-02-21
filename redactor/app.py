58f59841ea6ead350083e568302ac8f8d5429b44
ivanmilevtues <ivanmilevtues@gmail.com>
2017-02-21 15:01:55 -0800

    Adding save as functionality and also almost done open interface

--- redactor/app.py
+++ redactor/app.py
@@ -1,5 +1,12 @@
 from tkinter import *
 from redactor.TextEditor import TextEditor
+from menu.AppMenu import AppMenu
+from redactor.ResizingCanvas import ResizingCanvas
+
+# Move to setting file
+
+DEFAULT_WIDTH = 850
+DEFAULT_HEIGHT = 400
 
 
 def refresher(root):
@@ -11,6 +18,10 @@ def refresher(root):
 
 def main():
     editor = TextEditor()
+    ResizingCanvas(editor.get_text_panel(),
+                   width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT,
+                   bg="red")
+    AppMenu(editor.get_root(), editor.get_text_panel())
     editor.start()
 
 
