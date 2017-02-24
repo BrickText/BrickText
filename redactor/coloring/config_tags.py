510860f98cbd3422a5da2ab3a8631be77710ce72
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-02-24 15:55:31 +0200

    Add reset tags

--- redactor/coloring/config_tags.py
+++ redactor/coloring/config_tags.py
@@ -9,3 +9,12 @@ def config_tags(text_widget, language):
         text_widget.tag_config(k, foreground=v)
     text_widget.tag_config('blank', foreground='black')
     return keywords
+
+
+def reset_tags(text_widget, prev_language, language):
+    with open('settings/{}_keywords.json'.format(prev_language)) as data_file:
+        keywords = eval(data_file.read())
+    for k, v in keywords.items():
+        text_widget.tag_delete(k)
+
+    config_tags(text_widget, language)
